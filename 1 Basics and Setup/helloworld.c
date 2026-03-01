2ada6c27e9eae177740b9928bb502f6ee1cc35dc
Surya Ambati <39432528+Surya-Ambati@users.noreply.github.com>
2026-03-01 18:08:57 +0000

    uploading c file only

--- 1 Basics and Setup/helloworld.c
+++ 1 Basics and Setup/helloworld.c
@@ -1,7 +1,28 @@
-#include <stdio.h>
+#include<stdio.h>
+
+int x = 10;
 
 int main(){
-    printf("Hello World \n");
-    printf("--------------Programm End---------------\n");
-    return 0;
+
+printf("Hello World!!!\n");
+printf("-----------Programme End-----------------\n");
+return 0;
+
 }
+
+// single line comment
+
+/*
+Multiline comment:
+Behind the scences majorly perform 4 steps:
+
+1) preprocessing -> gcc -E helloworld.c
+2) compilation -> gcc -S helloworld.c - file.c (Assembly code)
+3) Assembling -> gcc -c file.c (machine code)
+4) Linking -> output - .o
+
+
+//memory/ RAM:
+1) text section 2) data section 3) heap 4) stack 
+
+*/
