83cd1f7ab389e196e8c16fa679061c6944b533f3
Surya Ambati <39432528+Surya-Ambati@users.noreply.github.com>
2026-03-16 15:55:21 +0000

    updated file fputs and fgets

--- Part 1/22 fputs_fgets.c
+++ Part 1/22 fputs_fgets.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
 
+//fgets(destination, size, stream); //stream - filepointer, stdin
+// - new line, \n, no data exists in file, size -1 character only.
+
 int main()
 {
-    FILE *fp;
+    FILE *fp = fopen("output.txt", "r");
+    char name[40];
 
-    fp = fopen("output.txt", "w");
+    // fputs("Hello World\n", fp);
+    // fputs("Welcome to C programming\n", fp);
 
-    fputs("Hello World\n", fp);
-    fputs("Welcome to C programming\n", fp);
+    //general string reading: 
+
+    // if(fp ==NULL){
+    //     perror("File Error");
+    // }
+    // else{
+    //     fgets(name, sizeof(name), fp );
+    //     printf("read data - %s", name);
+    // }
+
+    //complete string reading:
+    while(fgets(name, sizeof(name), fp) != NULL){
+        printf("inside data - %s", name);
+    }
 
     fclose(fp);
+
+    return 0;
 }
