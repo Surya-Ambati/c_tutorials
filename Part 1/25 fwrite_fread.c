45d7d058ffd97e879aed30615af7caf6272ce210
Surya Ambati <39432528+Surya-Ambati@users.noreply.github.com>
2026-03-17 15:37:58 +0000

    updated fwrite and read file

--- Part 1/25 fwrite_fread.c
+++ Part 1/25 fwrite_fread.c
@@ -7,7 +7,7 @@ int main()
     // int x = 65;
     // int arr[3] = {66,67, 68};
     int y;
-    // int arr[] = " ";
+    int arr[3];
 
 
     if(fp == NULL)
@@ -19,12 +19,16 @@ int main()
     // size_t result = fwrite(arr, sizeof(int), 3, fp);
     // printf("Items written: %zu\n", result);
 
-    fread(&y, sizeof(int), 1, fp);
-    printf("Value : %d\n", y);
+    fread(arr, sizeof(int), 3, fp);
+    // printf("Value : %d\n", y);
+    
+    for(int i=0; i<3; i++){
+        printf("value in array: %d\n", arr[i]);
+    }
 
     fclose(fp);
     return 0;
-    
+
 }
 
 //fprintf - text write, fwrite - binary write
