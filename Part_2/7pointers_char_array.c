29c9a81ad22841a219df36a031af66032f15cc71
Surya Ambati <asurya676@gmail.com>
2026-03-27 18:26:42 +0530

    updated char to pointer tutorials

--- Part_2/7pointers_char_array.c
+++ Part_2/7pointers_char_array.c
@@ -1,12 +1,23 @@
 #include<stdio.h>
+#include<string.h>
 
 
 int main(){
 
-    // char name[8] = {'s','u','r','y','a'};
-    // printf("name - %s\n", name);
-    int i = 10;
-    printf("test programme - %d times \n", i);
+  char name[6] = {'s','u','r','y','a','\0'}; 
+  printf("%s\n", name); 
+
+  int length = strlen(name); 
+  printf("lenght of name - %d\n", length); 
+  printf("size of name - %d\n", sizeof(name));
+
+char arrayname[] = "hello world !!!";
+printf("%s\n", arrayname); 
+
+char *pc = arrayname;
+printf("%p\n", arrayname);
+
+printf("accessing elements - %c\n", pc[2]); // pc[2] ==> *(pc +2)
 
 }
 
