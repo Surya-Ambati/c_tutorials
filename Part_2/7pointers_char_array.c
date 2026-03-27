6854819aa0a6ada3f6e07825efac20e4c25b5777
Surya Ambati <asurya676@gmail.com>
2026-03-27 17:36:50 +0530

    added new files and renamed folders

---
+++ Part_2/7pointers_char_array.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+
+int main(){
+
+    // char name[8] = {'s','u','r','y','a'};
+    // printf("name - %s\n", name);
+    int i = 10;
+    printf("test programme - %d times \n", i);
+
+}
+
+   
