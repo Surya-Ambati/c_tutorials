913c64400ede49148e20e9b990479aa82cc21246
Surya Ambati <asurya676@gmail.com>
2026-03-31 18:31:49 +0530

    added new tutorial

---
+++ Part_2/9pointers_functions.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+//stack generally has less memory and decide by operating system and compiler
+//limitations for stack memory - recursion function, large data allocate using array, rutime size allocation
+
+// 1) malloc - memory allocation: just allocate memory block and no initilalize. so if we dereference will provide garbage value.
+// 2) calloc - contiguous allocation: allocate & initialize memory ex: initializes with zero
+// 3) realloc - reallocation: resize existing memory 
+// 4) free - release allocated memory
+// must use stdlib to call pointer functions. these libary already define null as : #define NULL ((void*)0)
+
+
+int main(){
+
+//  doesnt work in runtime   
+// int n;
+// scanf("%d", &n);
+// int arr[n];
+
+
+int *ph;
+ph = (int*)malloc(sizeof(int));
+*ph = 50;
+printf("address that holds by ph - %d\n", ph);
+printf("address that contains value - %d\n", *ph);
+free(ph);
+ph = NULL;
+
+}
