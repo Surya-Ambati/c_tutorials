0e7c891cf0b9708db4602af180e9a59b0ecd2a7a
Surya Ambati <39432528+Surya-Ambati@users.noreply.github.com>
2026-06-30 10:44:16 +0000

    tutotial 20

--- cmake/include/math_lib.h
+++
@@ -1,6 +0,0 @@
-#ifndef MATH_LIB_H
-#define MATH_LIB_H
-
-int add_numbers(int a, int b);
-
-#endif
--- cmake/src/math_lib/math_lib.c
+++
@@ -1,8 +0,0 @@
-
-#include "math_lib.h"
-#include <stdio.h>
-
-int add_numbers(int a, int b){
-    printf("inside programme strated!!");
-    return a+b;
-}
